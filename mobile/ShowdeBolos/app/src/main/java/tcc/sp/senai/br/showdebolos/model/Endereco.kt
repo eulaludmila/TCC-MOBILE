ba426db087395a49a43f34a68db5e6ea2d0321bf
ALISSON ANGELO DE ANDRADE <18175302@CFP127ALUNO.LOCAL>
2019-09-09 16:52:43 -0300

    Versão 04 - Consumo de API, cadastro de confeiteiro ainda não testado

---
+++ mobile/ShowdeBolos/app/src/main/java/tcc/sp/senai/br/showdebolos/model/Endereco.kt
@@ -0,0 +1,11 @@
+package tcc.sp.senai.br.showdebolos.model
+
+data class Endereco (var cep: String,
+                var logradouro: String,
+                var bairro: String,
+                var cidade: String,
+                var uf: String ){
+
+
+
+}
